cc1e46c64bb8e02742dfc663dc3defbb162d3cbb
Silesh K Nair <sileshn@gmail.com>
2025-04-11 08:31:56 +0000

    redwood: Drop libhdrdynamic(ootf).so
    Used by nothing

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -516,8 +516,6 @@ PRODUCT_PACKAGES += \
     libgf_ca \
     libgf_hal \
     libgoodixhwfingerprint \
-    libhdrdynamic \
-    libhdrdynamicootf \
     libhta \
     libipebpsstriping \
     libipebpsstriping170 \
