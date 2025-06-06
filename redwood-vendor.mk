9251dac2d7eb1c84388877a85361c2ce6060672c
Silesh K Nair <sileshn@gmail.com>
2025-06-07 05:12:15 +0530

    redwood: camera: hook our java (jar) prebuilts as boot jars

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -1326,3 +1326,9 @@ PRODUCT_PACKAGES += \
     vendor_lib64_libEGL_adreno_so \
     vendor_lib64_libGLESv2_adreno_so \
     vendor_lib64_libq3dtools_adreno_so
+
+PRODUCT_BOOT_JARS += \
+    vendor.xiaomi.hardware.misys-V1.0-java \
+    vendor.xiaomi.hardware.misys-V2.0-java \
+    vendor.xiaomi.hardware.misys-V4.0-java \
+    vendor.xiaomi.hardware.misys.V3_0
