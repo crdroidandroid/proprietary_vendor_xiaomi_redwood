94ad588dd4704b4ed46f3ffb4d809dc69866b324
Silesh K Nair <sileshn@gmail.com>
2025-07-18 06:18:43 +0530

    Reapply "redwood: Setup firmware extraction"

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),redwood)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,77418a15dd4e320b57499849b290d8df1967e8cb)
+$(call add-radio-file-sha1-checked,radio/aop.img,5d2cf936434e343ad02c493ed43e93715a035b2b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,18fc04db6a5b8b53d19fb9d74b39bd7dea81b795)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,77ae33826766b455dfa5531b1fc31a2e2a78a08f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,a45c5db9c61e9f96e26b8acb08c73fd12b282162)
+$(call add-radio-file-sha1-checked,radio/dsp.img,d3fdbfa43b8686991d86c6679c3a87b07a11b368)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,d075198c5e559ccef14a14c7c1fe2af0a9bf9b2c)
+$(call add-radio-file-sha1-checked,radio/hyp.img,9cb3a3b086d5a429e00c22d803826811fcfc5464)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,87d9e025137b8292e4426c53107e7ccca5dd0a85)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,80c074ae35420082c0aec56da252531fcac87f39)
+$(call add-radio-file-sha1-checked,radio/modem.img,0b77099de9df83d01777a7b93965e62f7649a1c2)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,5ae8068bf8af54fd1fff36693e91d1201688158f)
+$(call add-radio-file-sha1-checked,radio/shrm.img,851d0aff07720762f19d49a78c87271c492630b8)
+$(call add-radio-file-sha1-checked,radio/tz.img,0afa81fae4cb7c29e73400a17f972a6952aad69a)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,ca443bf05c5164b3c6fa0e68e2d23c808de3598a)
+$(call add-radio-file-sha1-checked,radio/xbl.img,604274a5ffd48a3a36fd7d5338689560b2857329)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,a0bc29f5920e1fb988d4132c596ea01037ec8795)
+
+endif
